3707a005e57e5b58b3df5f55f2969c5f71c09ddc
Aleksey Zhidkov <aleksey.zhidkov@gmail.com>
2016-01-28 16:19:27 +0600

    Ex2-1: print ranges of standard type

---
+++ the_c_programming_language/chapter-2/dt_ranges.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <limits.h>
+
+void main() {
+  printf("%d\n", CHAR_MIN); 
+  printf("%d\n", CHAR_MAX); 
+  printf("%d\n", INT_MIN); 
+  printf("%d\n", INT_MAX); 
+  printf("%d\n", LONG_MIN); 
+  printf("%lu\n", LONG_MAX); 
+  printf("%d\n", SHRT_MIN); 
+  printf("%d\n", SHRT_MAX); 
+  printf("%lu\n", UINT_MAX); 
+  printf("%lu\n", ULONG_MAX); 
+}
